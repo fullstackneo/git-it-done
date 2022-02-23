63c1279d49255a54275e3c0ac48d86a4864d1bdb
Neo <fullstackneo@gmail.com>
2022-02-23 14:39:33 -0700

    add multiple page warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,15 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          var preEl = document.querySelector("#pre");
-          preEl.textContent = JSON.stringify(data, undefined, 2);
-
+    
           // pass response data to dom function
           displayIssues(data);
+
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo);
+          }
         });
       } else {
         alert("There was a problem with your request!");
@@ -20,7 +24,7 @@ var getRepoIssues = function (repo) {
     });
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues("angular/angular");
 
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -57,4 +61,17 @@ var displayIssues = function (issues) {
 
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
