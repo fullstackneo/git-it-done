abc1b3c7328f82e243e9f8e5ef730e6a891f618f
Neo <fullstackneo@gmail.com>
2022-02-23 22:43:40 -0700

    add sort by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,7 +5,7 @@
 //     });
 //   });
 // };
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 // getUserRepos();
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -98,3 +98,35 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
+
+var getFeaturedRepos = function (language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data.items, language);
+        });
+      } else {
+        ("Error: GitHub User Not Found");
+      }
+    })
+
+    .catch(function (error) {
+      console.log("Unable to connect");
+    });
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+  if (language) {
+    getFeaturedRepos(language);
+  }
+  // clear old content
+  repoContainerEl.textContent = "";
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
